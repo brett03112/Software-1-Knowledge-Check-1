c3fc42c87502eed5e96f3c74ab6b43d2d76395d5
Brett Smith <brett03112@gmail.com>
2024-08-12 03:01:36 -0400

    added files to .gitignore

--- KnowledgeCheck1_Calculator/obj/Debug/net8.0/KnowledgeCheck1_Calculator.AssemblyInfo.cs
+++ KnowledgeCheck1_Calculator/obj/Debug/net8.0/KnowledgeCheck1_Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnowledgeCheck1_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919dcb7a90e7f524bc98d75e3b0b3259f0effef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4081b556eff8653ea9e814df719baa35af7ce2c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnowledgeCheck1_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnowledgeCheck1_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
